255325ac6e704290ce2b47fa3da47bbadebb066c
nealdeters <nealdeters@gmail.com>
2016-04-14 19:46:48 -0500

    ordered questions api by id. added checkYesResponse model to angular

--- app/assets/javascripts/questions_ctrl.js
+++ app/assets/javascripts/questions_ctrl.js
@@ -3,23 +3,34 @@
 
  angular.module("app").controller("questionsCtrl", function($scope, $http){
    
-   $scope.setup = function(){
-       $http.get('/api/v1/questions.json').then(function(response){
-           $scope.questions = response.data;
-           console.log($scope.questions);
-       });
-   };
+    $scope.setup = function(){
+        $http.get('/api/v1/questions.json').then(function(response){
+            $scope.questions = response.data;
+            console.log($scope.questions);
+        });
+    };
 
-   $scope.question_index = 0;
-    $scope.question = {};
+    $scope.question_index = 0;
 
-    $scope.next = function () {
-        if ($scope.question_index >= $scope.questions.length - 1) {
-            $scope.question_index = 0;
-        } else {
-            $scope.question_index++;
-        }
-        console.log($scope.questions.length + '/' + $scope.question_index);
+    $scope.checkYesResponse = function(response) {
+      if (response == "landing") { 
+        $scope.question_index = 0
+      }
+      else {
+        $scope.question_index = response - 1
+      }
+    };
+
+    $scope.checkNoResponse = function(response) {
+      if (response == "faq") { 
+        return "faq";
+      }
+      else if (response !== "landing") {
+        return "landing";
+      }
+      else {
+        return question.id;
+      }
     };
 
 
