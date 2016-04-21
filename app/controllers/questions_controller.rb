f9f096003f55920918f6263d9c40359a2a04e998
Chris Connell <chris.g.connell@gmail.com>
2016-04-20 21:52:43 -0500

    index new, edit, delete buttons working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -23,13 +23,18 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new({text: params[:text], yes_response: params[:yes_response], no_response: params[:no_response], prequalifier: params[:prequalifier]})
     
-    
-    @question.save
+      @question.save
 
-    redirect_to '/questions_index'
+      if @question.save
+        flash[:success] = "New question created"
+        redirect_to '/questions_index'
+      else
+        flash[:warning] = "Question not saved"
+        redirect_to '/questions_index/new'
+      end
 
-    # authenicating
-    # type, 
+# how do we control that the new question fits in preceeds and proceeds the appropriate question?
+
   end
 
   def edit
@@ -43,6 +48,8 @@ class QuestionsController < ApplicationController
   def destroy
     @question = Question.find(params[:id])
     @question.destroy
+     flash[:success] = "Question deleted"
+        redirect_to '/questions_index'
   end 
 
 end
