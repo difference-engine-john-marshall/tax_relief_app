abac8d9dd6ad2ce0715de3d06728f6589fa0fe24
Cesar <cesarmejia451@gmail.com>
2016-04-21 19:52:09 -0500

    updated flash error messages on admin controller.

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,7 @@
 class AdminsController < ApplicationController
+  
   def new
+    @admin = Admin.new
   end
 
   def create
@@ -14,6 +16,8 @@ class AdminsController < ApplicationController
       redirect_to root_path
     else
       render :new
+
+
     end
   end
 end
