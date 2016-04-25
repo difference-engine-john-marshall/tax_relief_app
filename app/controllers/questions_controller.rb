301cf1e8f3f36a8005e8516214d54e3753d9a8b0
Chris Connell <chris.g.connell@gmail.com>
2016-04-25 11:57:43 -0500

    can save new, can destroy, edit not saving

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -22,7 +22,7 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new({text: params[:text], yes_response: params[:yes_response], no_response: params[:no_response], prequalifier: params[:prequalifier]})
-    
+    session[:return_to] = request.referer
       @question.save
 
       if @question.save
@@ -30,11 +30,8 @@ class QuestionsController < ApplicationController
         redirect_to '/questions_index'
       else
         flash[:warning] = "Question not saved"
-        redirect_to '/questions_index/new'
+        redirect_to request.referer
       end
-
-# how do we control that the new question fits in preceeds and proceeds the appropriate question?
-# verify yes_response is unique?
 
   end
 
@@ -43,14 +40,14 @@ class QuestionsController < ApplicationController
   end 
 
   def update
-    @questions = Question.update({text: params[:text], qualifying_response: params[:qualifying_response], next_question_id: params[:next_question_id]})
+    @question.update({text: params[:text], yes_response: params[:yes_response], no_response: params[:no_response], prequalifier: params[:prequalifier]})
   end
 
   def destroy
     @question = Question.find(params[:id])
     @question.destroy
-     flash[:success] = "Question deleted"
-        redirect_to '/questions_index'
+    flash[:success] = "Question deleted"
+    redirect_to '/questions_index'
   end 
 
 end
