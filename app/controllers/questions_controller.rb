5aebf64d23c7dc1043102d34851121b9abd65e4d
Chris Connell <chris.g.connell@gmail.com>
2016-04-25 20:12:36 -0500

    admin validation working for questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  # before_action :authenticate_admin!
+  before_action :authenticate_admin!, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   def home
 
